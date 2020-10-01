a2a99e054e7f10d0212e6e2ec7d926b15cda37f7
Gerald-Izuchukwu <geraldlouisugwunna@gmail.com>
2020-10-01 23:46:26 +0100

    Created a simple python calculator

---
+++ Simple_Caluclator/simple_calculator.py
@@ -0,0 +1,18 @@
+#This simple calculator, calculates with the fiur basic operators, Addition "+",
+# Subtraction "-", Multiplication "*" and Division "/"
+
+num1 = float(input ("enter your first number: "))
+#float actually converted whatever the user inputed from a string to a number, either integer or decimal
+op = input ("enter your operator ")
+num2 = float(input ("enter your second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
+else:
+    print("invalid operator")
