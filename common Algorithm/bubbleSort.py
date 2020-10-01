f439f8980caf33fde35ead16e72b4c33437fa88a
jigmet12 <jigmetashi02@gmail.com>
2020-10-02 03:25:56 +0400

    added bubble sort

---
+++ common Algorithm/bubbleSort.py
@@ -0,0 +1,16 @@
+def bubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+
+arr = [6, 8, 10, 11]
+
+bubbleSort(arr)
+
+print("Array sorted:")
+for i in range(len(arr)):
+    print("%d" % arr[i]),
