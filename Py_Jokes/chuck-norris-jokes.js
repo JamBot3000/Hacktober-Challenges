657dfc09cd920a966759e926723d499d4c5400b4
Milos Vujinic <milos.vujinic2@gmail.com>
2020-10-02 02:39:48 +0200

    Added running instructions

--- Py_Jokes/chuck-norris-jokes.js
+++ Py_Jokes/chuck-norris-jokes.js
@@ -1,10 +1,13 @@
 const https = require('https')
 const API_URL = 'https://api.chucknorris.io/jokes/random'
 
+/**
+ * To run this you will need node installed
+ * Run by executing `node chuck-norris-jokes.js`
+ * @author milos5593
+ */
 function GET_JOKES() {
     const req = https.get(API_URL, (res) => {
-        console.log(`statusCode: ${res.statusCode}`)
-
         res.on('data', (d) => {
             console.log(JSON.parse(d.toString()).value)
         })
