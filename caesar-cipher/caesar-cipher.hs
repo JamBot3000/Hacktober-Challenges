7f5dd190b28117f1cb457d21345cc0d5a591cb2f
Francesco Migliaro <39830676+Pur2all@users.noreply.github.com>
2020-10-03 00:39:38 +0200

    Update caesar-cipher.hs

--- caesar-cipher/caesar-cipher.hs
+++ caesar-cipher/caesar-cipher.hs
@@ -1,5 +1,5 @@
 import Data.Char
 
-crypt message key = concatMap (\el -> let round = if isUpper el then 65 else 97 in [toEnum $ key + fromEnum el ::Char]) message
+crypt message key = concatMap (\el -> [toEnum $ key + fromEnum el ::Char]) message
 
 decrypt message key = crypt message $ negate key
