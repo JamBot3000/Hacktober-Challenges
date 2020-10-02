66c5b381630b6e16eed897b352fe0259670629af
Ayush Raj <beast@localhost.localdomain>
2020-10-02 09:33:46 +0530

    added a simple calculator react app

---
+++ SimpleCalculator_React_APP/src/component/button.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Button extends Component{
+
+    render(){
+        return(
+            <div className={`column-${this.props.cols}`}>
+                <button className= "calc-button" onClick={()=>this.props.action(this.props.symbol)}>{this.props.symbol}</button>
+            </div>
+        );
+    }
+}
+
+export default Button;
