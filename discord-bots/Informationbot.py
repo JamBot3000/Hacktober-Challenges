5496e2613f4b6a4e2da380b42451fb3ae5bd26d3
MeFaltaUnVerano <trolleros200@gmail.com>
2020-10-03 20:05:08 +0200

    Update Informationbot.py

--- discord-bots/Informationbot.py
+++ discord-bots/Informationbot.py
@@ -1,14 +1,6 @@
 import discord
 from discord.ext import commands
 import discord.utils
-
-
-
-
-
-
-
-TOKEN = ''
 client = discord.Client()
 client = commands.Bot(command_prefix=BOT_PREFIX)
 
@@ -16,16 +8,9 @@ client = commands.Bot(command_prefix=BOT_PREFIX)
 @commands.guild_only()
 async def infobot(self, ctx):
     embed = discord.Embed(title="INFO BOT", color=0x00e1ff)
-    embed.add_field(name=str("GUILDS"),
-                    value=f'`{len(client.guilds)}`',
-                    inline=False)
-    embed.add_field(name=str("LATENCY"),
-                    value=f'`{round(client.latency * 1000)} ms`',
-                    inline=False)
-    embed.add_field(name=str("Total Commands"),
-                    value=f'`{len(client.commands)}`',
-                    inline=False)
+    embed.add_field(name=str("GUILDS"), value=f'`{len(client.guilds)}`', inline=False)
+    embed.add_field(name=str("LATENCY"),value=f'`{round(client.latency * 1000)} ms`',inline=False)
+    embed.add_field(name=str("Total Commands"), value=f'`{len(client.commands)}`', inline=False)
     await ctx.send(embed=embed)
 
-client.run(TOKEN, reconnect=True)
-
+client.run('token here', reconnect=True)
