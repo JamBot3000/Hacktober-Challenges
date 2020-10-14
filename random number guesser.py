f48462ca206c3eeb50a8dce87af3204fb52145e6
anshulchaudhary <54776396+anshulchaudhary0677@users.noreply.github.com>
2020-10-14 10:01:59 +0530

    Update random number guesser.py
    Updated the game a little bit, where now user will know if he/she has guessed a smaller number or a larger number  to an original number generated so as to proceed to guess a correct random number and added the total number of chances that user will get, i.e. 10 chances so make the game little more trickier and better .

--- random number guesser.py
+++ random number guesser.py
@@ -1,19 +1,24 @@
-import random
-
-goAgain = True
-
-while goAgain == True:
-    randomNum = random.randint(0, 100)
-    userGuess = input("\nPlease guess the number: ")
-    
-    if userGuess == str(randomNum):
-        userGoAgain = input("\nYou guessed the number! Would you like to go again? (y/n)")
-
-        if str(userGoAgain) == "y":
-            goAgain = True
+def choose_rand():
+    import random
+    rand_num = random.randint(0,100)
+    i = 0
+    while i < 10:
+        input_num = int(input("Guess a Number and Enter that Number here : "))
+        if input_num < rand_num :
+            print('the number you have guessed is smaller, please guess a larger number')
+            i = i + 1
+        
+        elif input_num > rand_num : 
+            print('the number you have guessed is bigger, please guess a smaller number')
+            i = i + 1
+            
+            
         else:
-            goAgain = False
-    else:
-        print("You guessed wrong! Try again")
-    
-print("\nThanks for playing")
+            print('Congrats you have guessed the correct number :) in chances : ',i)
+            i = 11
+    if i == 10:
+        
+        print('sorry better luck next time, the number was :  ',rand_num)
+        
+
+choose_rand()
